d49349afd5a3b36d7c2ef562848497e06543f1c4
Ariel Molho <arielmolho@gmail.com>
2021-01-23 16:07:09 -0300

    Ajustes en script.js para primer entrega proyecto

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,7 @@ function spaceShip(argName, argModel, argAge) {
     };
     this.getEvaluation = function () {
         if (this.age > 10) {
-            return `Su nave ${this.model}, ya no es asegurable`;
+            return `Su nave ${this.model}, ya no es asegurable. Que la fuerza te acompañe!`;
         } else {
             return `Su nave ${this.model} es una nave asegurable`;
         }
@@ -70,7 +70,7 @@ switch(infoSolicitante[1]){
         valorNave = (100000-(100000*infoSolicitante[2]*0.1));
         break;
     default:
-        console.log("Su nave no es asegurable. Que la fuerza esté contigo!");
+        console.log("Su nave no se encuentra en nuestra base de datos. Por favor contacte a un asesor para una propuesta personalizada.");
         break;
 }
 
@@ -79,7 +79,54 @@ infoSolicitante.push(valorNave);
 console.log(infoSolicitante.length);
 console.log(infoSolicitante[3]);
 
-//PROXIMOS PASOS: Cálculo póliza
+var seguroTipo = prompt("¿Qué tipo de cobertura desea contratar? (Bronce, Plata, Oro, Platino)").toUpperCase();
+var porcentajeAsegurable;
+//por ahora lo uso con console.log para no saturar de alerts y prompts
+switch(seguroTipo){
+    case "BRONCE":
+        porcentajeAsegurable = 0.5;
+        break;
+    case "PLATA":
+        porcentajeAsegurable = 0.65;
+        break;
+    case "ORO":
+        porcentajeAsegurable = 0.85;
+        break;
+    case "PLATINO":
+        porcentajeAsegurable = 1;
+        break;
+    default:
+        console.log("No ha seleccionado una covertura que podamos ofrecer. Por favor contacte a un asesor para una propuesta personalizada.");
+        break;
+}
+
+infoSolicitante.push(porcentajeAsegurable);
+
+var valorAsegurable = (infoSolicitante[3]*infoSolicitante[4]);
+console.log(valorAsegurable);
+infoSolicitante.push(valorAsegurable);
+var primaAnual = ((infoSolicitante[5]/10)*(infoSolicitante[4]/10));
+console.log(primaAnual);
+infoSolicitante.push(primaAnual);
+
+//Se que esto queda fatal en el HTML pero quería probarlo
+document.write(
+    `Estimado/a ${infoSolicitante[0]}, <br>
+    Gracias por confiar en Spaceballs Seguros ©. A continuación le detallamos la información de su póliza: <br>
+    - Nave: ${infoSolicitante[1]} <br>
+    - Antigüedad: ${infoSolicitante[2]} <br>
+    - Valor Asegurable: ${infoSolicitante[3]} <br>
+    <br>
+    Habiendo seleccionado la póliza ${seguroTipo}, la cobertura máxima es del ${(infoSolicitante[4]*100)}%. <br>
+    Su prima anual será de ${infoSolicitante[6]} de créditos. <br>
+    <br>
+    Le agradecemos la confianza en Spaceballs Seguros © <br>
+    <br>
+    Que la fuerza lo acompañe y larga vida al Imperio.
+    `
+);
+
+//Cálculo póliza
 //valor residual asegurable = valor nave - depreciación por antiguedad
 //valor a segurar = valor asegurable * porcentaje a asegurar según póliza seleccionada
 //prima = valor a segurar * porcentaje que se considera de reaseguro anual
