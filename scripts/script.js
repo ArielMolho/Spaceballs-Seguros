28ac0a49f716d2ee108560a0b63255acffcb7348
Ariel Molho <arielmolho@gmail.com>
2021-02-23 15:33:00 -0300

    adding jquery

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,10 @@
 var validation;
 (function() {
     'use strict';
-    window.addEventListener('load', function() {
-        var forms = document.getElementsByClassName('needs-validation');
+    $(document).ready(function() {
+        var forms = $('.needs-validation');
         validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
+            form.addEventListener('submit', function(event){
                 event.preventDefault();
                 if (form.checkValidity() === false) {
                     event.stopPropagation();
@@ -18,54 +18,50 @@ var validation;
 })();
 
 // Inicialización
-var nombre = document.getElementById("name");
-var email = document.getElementById("email");
-var modelo = document.getElementById("formSelectShip");
-var modelSelected;
-var antiguedad = document.getElementById("formSelectAge");
-var ageSelected;
-var seguroTipo = document.getElementById("formSelectCoverage");
-var insuranceSelected;
+var nombre;
+var email;
+var modelo;
+var antiguedad;
+var seguroTipo;
 var valorNave;
 var valorAsegurable;
 var porcentajeAsegurable;
 var primaAnual;
-var cotizarBoton = document.getElementById('cotizarBoton');
 var infoSolicitante;
 
 // Eventos y Funciones
 function valorResidualNave (){
     // Cálculo de póliza: valor residual asegurable = valor nave - depreciación por antiguedad
-    if (modelSelected == 'Star Destroyer') {                
+    if (modelo == 'Star Destroyer') {                
         console.log("El valor de esta nave nueva 150 millones de créditos y tiene una depreciación anual del 10%");
-        valorNave = (150000000-(150000000*ageSelected*0.1));
-    } else if (modelSelected == 'Dreadnought Super Star Destroyer') {                
+        valorNave = (150000000-(150000000*antiguedad*0.1));
+    } else if (modelo == 'Dreadnought Super Star Destroyer') {                
         console.log("El valor de esta nave nueva 325.000 millones de créditos y tiene una depreciación anual del 10%");
-        valorNave = (325000000000-(325000000000*ageSelected*0.1));
-    } else if (modelSelected == 'Mon Calamari Star Cruiser') {                
+        valorNave = (325000000000-(325000000000*antiguedad*0.1));
+    } else if (modelo == 'Mon Calamari Star Cruiser') {                
         console.log("El valor de esta nave nueva 88 millones de créditos y tiene una depreciación anual del 10%");
-        valorNave = (88000000-(88000000*ageSelected*0.1));
-    } else if (modelSelected == 'Corellian Corvette') {                
+        valorNave = (88000000-(88000000*antiguedad*0.1));
+    } else if (modelo == 'Corellian Corvette') {                
         console.log("El valor de esta nave nueva 3.5 millones de créditos y tiene una depreciación anual del 10%");
-        valorNave = (3500000-(3500000*ageSelected*0.1));
-    } else if (modelSelected == 'Corellian Hammerhead Corvette') {   
+        valorNave = (3500000-(3500000*antiguedad*0.1));
+    } else if (modelo == 'Corellian Hammerhead Corvette') {   
         console.log("El valor de esta nave nueva 1.5 millones de créditos y tiene una depreciación anual del 10%");
-        valorNave = (1500000-(1500000*ageSelected*0.1));
-    } else if (modelSelected == 'Corellian Freighter') {   
+        valorNave = (1500000-(1500000*antiguedad*0.1));
+    } else if (modelo == 'Corellian Freighter') {   
         console.log("El valor de esta nave nueva 100.000 créditos y tiene una depreciación anual del 10%");
-        valorNave = (100000-(100000*ageSelected*0.1));
+        valorNave = (100000-(100000*antiguedad*0.1));
     }
-}
+};
 
 function procesarPoliza (){
     valorResidualNave ();
-    if(insuranceSelected == "Bronce") {
+    if(seguroTipo == "Bronce") {
         porcentajeAsegurable = 0.5;
-    } else if (insuranceSelected == "Plata") {
+    } else if (seguroTipo == "Plata") {
         porcentajeAsegurable = 0.65;
-    } else if (insuranceSelected == "Oro") {
+    } else if (seguroTipo == "Oro") {
         porcentajeAsegurable = 0.85;
-    } else if (insuranceSelected == "Platino") {
+    } else if (seguroTipo == "Platino") {
         porcentajeAsegurable = 1;
     }
     
@@ -74,17 +70,18 @@ function procesarPoliza (){
     
     //Cálculo de póliza: prima = valor a segurar * porcentaje que se considera de reaseguro anual
     primaAnual = ((valorAsegurable/10)*(porcentajeAsegurable/10));
-}
+    storage();
+};
 
 function procesarFormulario (){
-    cotizarBoton.addEventListener('click', function(event){
-        nombre = nombre.value;
-        email = email.value;
-        modelSelected = modelo.options[modelo.selectedIndex].value;
-        ageSelected = antiguedad.options[antiguedad.selectedIndex].value;
-        insuranceSelected = seguroTipo.options[seguroTipo.selectedIndex].value;
-        if (ageSelected >= 10) {
-            alert(`${nombre}, su nave ${modelSelected}, ya no es asegurable. Que la fuerza te acompañe!`);
+    $("#cotizarBoton").click( function(){
+        nombre = $("#name").val();
+        email = $("#email").val();
+        modelo = $("#formSelectShip").val();
+        antiguedad = $("#formSelectAge").val();
+        seguroTipo = $("#formSelectCoverage").val();
+        if (antiguedad >= 10) {
+            alert(`${nombre}, su nave ${modelo}, ya no es asegurable. Que la fuerza te acompañe!`);
         } else {
             procesarPoliza ();
             
@@ -92,11 +89,11 @@ function procesarFormulario (){
             infoSolicitante = {
                 "nombre": nombre,
                 "email": email,
-                "modelo": modelSelected,
-                "antiguedad": ageSelected,
+                "modelo": modelo,
+                "antiguedad": antiguedad,
                 "valorNave": valorNave,
                 "asegurable": valorAsegurable,
-                "poliza": insuranceSelected,
+                "poliza": seguroTipo,
                 "porcentajeAsegurable": porcentajeAsegurable,
                 "prima": Math.round(primaAnual)
             };
@@ -105,13 +102,11 @@ function procesarFormulario (){
             modal();
         }
     })
-}
+};
 
 function modal(){
-    var myContenedor = document.getElementById("modal-body");
-    var myPoliza = document.createElement('p');
-    myPoliza.innerHTML = 
-        `Estimado/a ${infoSolicitante["nombre"]}, <br>
+    $("#modal-body").append(
+        `<p>Estimado/a ${infoSolicitante["nombre"]}, <br>
         Gracias por confiar en Spaceballs Seguros ©. A continuación le detallamos la información de su póliza: <br>
         - Nave: ${infoSolicitante["modelo"]} <br>
         - Antigüedad: ${infoSolicitante["antiguedad"]} <br>
@@ -122,7 +117,12 @@ function modal(){
         <br>
         Le agradecemos la confianza en Spaceballs Seguros © <br>
         <br>
-        Que la fuerza lo acompañe y larga vida al Imperio.`;
-    myContenedor.appendChild(myPoliza);
+        Que la fuerza lo acompañe y larga vida al Imperio.</p>`
+    );
     $("#staticBackdrop").modal({show: true});
-}
+};
+
+function storage (){
+    sessionStorage.Name = infoSolicitante["nombre"];
+    sessionStorage.Email = infoSolicitante["email"];
+};
